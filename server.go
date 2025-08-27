cbf2b10e90c9452f1f934b7c9bf429fa864a5f78
Daiana L <daianalimaep@gmail.com>
2025-08-27 15:49:37 -0300

    feat: add health, secret, and configmap endpoints with probes and env integration

--- server.go
+++ server.go
@@ -6,9 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/secret", secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
@@ -33,4 +37,17 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Erro reading file: %v", err)
 	}
 	fmt.Fprintf(w, "My family: %s.", string(data))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
